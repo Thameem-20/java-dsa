ca7db392e697498a1499c6ad3eb51beb20572e80
Thameem-20 <ahmedthameem20@gmail.com>
2024-07-05 22:47:24 +0530

    updated

---
+++ CharStringConversion.java
@@ -0,0 +1,21 @@
+public class CharStringConversion {
+
+    public static void main(String[] args) {
+   
+
+   
+    char ch = 'A';
+   
+    String str = Character.toString(ch);
+   
+    System.out.println("Char to String: " + str);
+   
+
+   
+    String s = "Hello";
+   
+    char c = s.charAt(0);
+   
+    System.out.println("String to Char: " + c);
+   
+    }
